0bc4c21c55970653cdc06db0d34f1d41b4d064fb
Nailson <nailson_ferreira@hotmail.com>
2019-03-26 12:10:44 -0300

    Agenda, botões, ação voltar

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,34 @@
+package br.com.alura.agenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+//Cria e instancia um array de strings, transforma em listview, adapta esta lista para
+        String [] alunos = {"Daniel","Ronaldo", "Jeferson", "Felipe", "João","Ariadna", "Sidney", "Stephen", "Susan", "Collins"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = (Button) findViewById (R.id.novo_aluno);
+
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)  {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+    }
+}
