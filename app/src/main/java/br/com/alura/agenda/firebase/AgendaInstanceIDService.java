add90cfdf5dbff12bb28392a6951c517123bbce5
Nailson <nailson_ferreira@hotmail.com>
2019-05-06 10:18:25 -0300

    Sem gerar Token/Nova implementacao

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIDService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIDService.java
@@ -2,20 +2,34 @@ package br.com.alura.agenda.firebase;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class AgendaInstanceIDService extends FirebaseInstanceIdService{
+import br.com.alura.agenda.retrofit.RetrofitInicializador;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AgendaInstanceIDService extends FirebaseMessagingService {
 
     @Override
-    public void onTokenRefresh() {
-
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d("Token Firebase", "Refreshed token: " + refreshedToken);
-
-        enviaTokenParaServidor(refreshedToken);
+    public void onNewToken(String token) {
+        Log.d("token firebase", "Refreshed token: " + token);
+        // TODO: Implement this method to send any registration to your app's servers.
+        enviaTokenParaServidor(token);
     }
 
-    private void enviaTokenParaServidor(String refreshedToken) {
+    private void enviaTokenParaServidor(final String token) {
+        Call<Void> call = new RetrofitInicializador().getDispositivoService().enviarToken(token);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.i("token enviado", token);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("token falhou", t.getMessage());
+            }
+        });
     }
 }
